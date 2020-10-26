621909bc979722dca855075a7203f39b9d3b0428
Aleksey-Kirilov <aleks.kirilov2015@yandex.ru>
2020-10-26 19:53:27 +0500

    Добавьте файлы проекта.

---
+++ 26.10 6 вариант/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _26._10_6_вариант
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int firstNum = int.Parse(TxtNum1.Text);
+            int secondNum = int.Parse(TxtNum2.Text);
+            if (firstNum%secondNum == 0)
+            LblResult.Content = "Число делится без остатка\nЧастное: " + (firstNum / secondNum).ToString(); 
+            else
+                LblResult.Content = "Число делится с остатком\nЧастное: " + (firstNum / secondNum).ToString() + "\n Остаток:" + (firstNum % secondNum).ToString();
+        }
+    }
+}
